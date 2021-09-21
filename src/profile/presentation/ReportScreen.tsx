bcff80e484d901751b6ff5a5d940f96ca7f38261
stormborax <stormborax@gmail.com>
2021-09-21 12:50:15 +0700

    filled edit language and report content

--- src/profile/presentation/ReportScreen.tsx
+++ src/profile/presentation/ReportScreen.tsx
@@ -1,5 +1,15 @@
 import React from "react";
-import { Box, Text } from "native-base";
+import {
+  Box,
+  Text,
+  Input,
+  TextArea,
+  ScrollView,
+  HStack,
+  Button,
+  Divider,
+} from "native-base";
+import { MaterialIcons, FontAwesome, Ionicons } from "@expo/vector-icons";
 
 const ReportScreen: React.FC = ({ navigation, route }) => {
   React.useLayoutEffect(() => {
@@ -8,10 +18,73 @@ const ReportScreen: React.FC = ({ navigation, route }) => {
     });
   });
   return (
-    <Box px={4} pt={4} flex={1} alignItems="center" bg="white">
-      <Text fontSize="2xl" color="grey" bold mt={4}>
-        {route.name}
-      </Text>
+    <Box flex={1}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        px={4}
+        pt={4}
+        flex={1}
+        bg="white"
+      >
+        <HStack p={6}>
+          <Text position="absolute" left={0} mt={3} ml={2}>
+            Subjek
+          </Text>
+          <HStack position="absolute" right={0} mt={3} mr={2}>
+            <Text>Pilih Subjek</Text>
+            <MaterialIcons
+              name="keyboard-arrow-right"
+              size={24}
+              color="black"
+            />
+          </HStack>
+        </HStack>
+        <Divider />
+        <HStack p={6}>
+          <Text position="absolute" left={0} ml={2} mt={3}>
+            Outlet
+          </Text>
+          <HStack position="absolute" right={0} mr={2} mt={3}>
+            <Text>Pilih Outlet</Text>
+            <MaterialIcons
+              name="keyboard-arrow-right"
+              size={24}
+              color="black"
+            />
+          </HStack>
+        </HStack>
+        <Divider />
+
+        <Text mt={2} fontSize="sm">Email</Text>
+        <Input
+          my={2}
+          w="100%"
+          variant="filled"
+          value="stormborax@gmail.com"
+          size="xs"
+        />
+        <Text mt={2} fontSize="sm">
+          Pesan
+        </Text>
+        <TextArea
+          my={2}
+          w="100%"
+          textAlignVertical="top"
+          variant="filled"
+          placeholder="Jangan Bete"
+          size="xs"
+        />
+      </ScrollView>
+      <HStack safeAreaBottom justifyContent="center" py={3} bgColor="white">
+        <Button
+          w="95%"
+          bg="#BA2025"
+          colorScheme="orange"
+          _text={{ fontWeight: "bold", fontSize: "lg" }}
+        >
+          KIRIM
+        </Button>
+      </HStack>
     </Box>
   );
 };
