b7f9e854af985c70416228b91cc3a5e20364e8be
rakhanaf <rnaufalaf@gmail.com>
2021-09-27 13:04:33 +0700

    initial catalogue layout added

--- src/catalogue/presentation/CatalogueScreen.tsx
+++ src/catalogue/presentation/CatalogueScreen.tsx
@@ -1,10 +1,31 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { ScrollView } from "react-native";
+import { Box, Divider, HStack, Heading } from "native-base";
+
+import CatalogueScrollableContainer from "../../core/presentation/CatalogueScrollableContainer";
+import PromoCard from "../../core/presentation/PromoCard";
 const CatalogueScreen: React.FC = ({ route }) => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>{route.name}</Text>
-    </View>
+    <ScrollView>
+      <Box flex={1} bg="white">
+        <CatalogueScrollableContainer />
+        <Divider />
+        <Box>
+          <Heading size="md" px={4} py={2} color="black">
+            Promo spesial untukmu
+          </Heading>
+          <ScrollView horizontal={true}>
+            <HStack space={3}>
+              <PromoCard />
+              <PromoCard />
+              <PromoCard />
+              <PromoCard />
+              <PromoCard />
+            </HStack>
+          </ScrollView>
+        </Box>
+      </Box>
+    </ScrollView>
   );
 };
 
